7b8d2c2227ef83acda487c6665db4b3509e24539
demonsofya <148910070+demonsofya@users.noreply.github.com>
2025-08-23 10:19:31 +0300

    Добавлены enum и функции для квадратных и линейных уравнений, добавлены объявления всех функций

--- scareuravnenie/main.c
+++ scareuravnenie/main.c
@@ -3,81 +3,119 @@
 #include <assert.h>
 //#include <TXLib.h>
 
-const int INF = -1; /* roots = infinity */
+bool CompareDoubles (double a, double b);
+int Linear(double a, double b, double c, double *x1, double *x2);
+int Quadr(double a, double b, double c, double *x1, double *x2);
+int SqrEq(double a, double b, double c, double *x1, double *x2);
+int scan(double *a, double *b, double *c);
+int print(int nRoots, double x1, double x2);
+int print(int nRoots, double x1, double x2);
+
+
+enum ROOTS {
+    INF = -1,
+    NoRoots = 0,
+    OneRoot = 1,
+    TwoRoots = 2
+        // TODO: enum
+};
+
+const double eps = 0.001;
 
 bool CompareDoubles (double a, double b) {   /*сравнение даблов */
-    if (fabs(a - b) < 0.001)
+    if (fabs(a - b) < eps) //TODO: const
         return true;
+
     return false;
 }
 
-int SqrEq(double a, double b, double c,
-          double *x1, double *x2) { /* функция, которая решает квадратное уравнение */
+int Linear(double a, double b, double c,
+          double *x1, double *x2) {
+    // assetов нет, они в SqrEq
 
-    assert (*x1 == NULL);
-    assert (*x2 == NULL);      /* ошибочки */
-    assert (*x1 == *x2);
+    if (CompareDoubles(b, 0)) {       // TODO: func
+        if (CompareDoubles(c, 0))
+            return INF;
 
-    if (CompareDoubles(a, 0)) { /* вариант, когда уравнение линейное */
-        if (CompareDoubles(b, 0)) {
-            if (CompareDoubles(c, 0)) {
-                return INF;
-            }
-
-            return 0;
-        }
-
-        *x1 = - c / b;
-        return 1;
+        return NoRoots;
     }
+    *x1 = - c / b;
 
+    return OneRoot;
+}
+
+int Quadr(double a, double b, double c,
+          double *x1, double *x2) {
     double d = b * b - 4 * a * c; /* дискриминант */
 
     if (d < 0) {
-        return 0;
+        return NoRoots;
     }
     if (CompareDoubles(d, 0)) {
         *x1 = - b / (2 * a);
-        return 1;
+        return OneRoot;
     }
 
     double sqr = sqrt(d);
     *x1 = (-b + sqr) / (2 * a);
     *x2 = (-b - sqr) / (2 * a);
 
-    return 2;
+    return TwoRoots;
+}
+
+int SqrEq(double a, double b, double c,
+          double *x1, double *x2) { /* функция, которая решает квадратное уравнение */
+
+    assert (x1 != NULL);
+    assert (x2 != NULL);      /* ошибочки */
+    assert (*x1 == *x2);
+
+    if (CompareDoubles(a, 0)) { /* вариант, когда уравнение линейное */
+        return Linear(a, b, c, x1, x2);
+    }
+
+    return Quadr(a, b, c, x1, x2);
 }
 
 int scan(double *a, double *b, double *c) {
 
+    assert (a != NULL);
+    assert (b != NULL);
+    assert (c != NULL);
+
     printf("The quadratic equation solver\n");
     printf("Enter three number: \n");
     scanf("%lf %lf %lf", a, b, c);
-    return 0;
 
+    return 0;
 }
 
 int print(int nRoots, double x1, double x2) {
 
-    if (nRoots == 0) {
-        printf("No Roots\n");
-        return 0;
+    switch (nRoots) {
+        case NoRoots:
+            printf("No Roots\n");
+            return 0;
+            break;
+        case OneRoot:
+            printf("x = %f\n", x1);
+            return 0;
+            break;
+        case TwoRoots:
+            printf("x1 = %f, x2 = %f\n", x1, x2);
+            return 0;
+            break;
+        case INF:
+            printf("Infinity\n");
+            return 0;
+            break;
+        default:
+            printf ("Error");
+            return 1;
+            break;
     }
-    if (nRoots == 1) {
-        printf("x = %f\n", x1);
-        return 0;
-    }
-    if (nRoots == INF) {
-        printf("Infinity\n");
-        return 0;
-    }
-    if (nRoots == 2) {
-        printf("x1 = %f, x2 = %f\n", x1, x2);
-        return 0;
-    }
-    printf ("Error");
-    return 1;
 
+    return 1;
 }
 
 int main(void) {
@@ -86,12 +124,12 @@ int main(void) {
     double x1 = 0, x2 = 0;
 
     int flag = scan(&a, &b, &c);
-    printf("%f %f %f\n", a, b, c);
+    printf("%lf %lf %lf\n", a, b, c);
 
     int nRoots = SqrEq(a, b, c, &x1, &x2);
-    printf("%g %g %d\n", x1, x2, nRoots);
+    printf("%lf %lf %d\n", x1, x2, nRoots);
 
     flag = print(nRoots, x1, x2);
-    return 0;
 
+    return 0;
 }
